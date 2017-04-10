95116f99b5542d7b1765a18f15ae585dc6fa894d
Pranav Suri <pranavsuri1127@gmail.com>
2017-04-10 23:22:57 +0530

    updated

--- imageprocessing/Form1.Designer.cs
+++ imageprocessing/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,17 +37,20 @@
             this.imageProcessingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.greyScaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extractRedChannelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thresholdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.extractRedChannelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -56,7 +60,7 @@
             this.imageProcessingToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(821, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(876, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -73,21 +77,21 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
@@ -96,7 +100,8 @@
             this.imageProcessingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.greyScaleToolStripMenuItem,
             this.grayscaleToolStripMenuItem,
-            this.extractRedChannelToolStripMenuItem});
+            this.extractRedChannelToolStripMenuItem,
+            this.thresholdToolStripMenuItem});
             this.imageProcessingToolStripMenuItem.Name = "imageProcessingToolStripMenuItem";
             this.imageProcessingToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
             this.imageProcessingToolStripMenuItem.Text = "Image Processing";
@@ -104,65 +109,16 @@
             // greyScaleToolStripMenuItem
             // 
             this.greyScaleToolStripMenuItem.Name = "greyScaleToolStripMenuItem";
-            this.greyScaleToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.greyScaleToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
             this.greyScaleToolStripMenuItem.Text = "Sepia";
             this.greyScaleToolStripMenuItem.Click += new System.EventHandler(this.greyScaleToolStripMenuItem_Click);
             // 
             // grayscaleToolStripMenuItem
             // 
             this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
-            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
             this.grayscaleToolStripMenuItem.Text = "Grayscale";
             this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(821, 237);
-            this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(404, 211);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox2.Location = new System.Drawing.Point(413, 3);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(405, 211);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(162, 7);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
-            this.trackBar1.TabIndex = 4;
             // 
             // extractRedChannelToolStripMenuItem
             // 
@@ -171,12 +127,72 @@
             this.extractRedChannelToolStripMenuItem.Text = "Extract Red Channel";
             this.extractRedChannelToolStripMenuItem.Click += new System.EventHandler(this.extractRedChannelToolStripMenuItem_Click);
             // 
+            // thresholdToolStripMenuItem
+            // 
+            this.thresholdToolStripMenuItem.Name = "thresholdToolStripMenuItem";
+            this.thresholdToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.thresholdToolStripMenuItem.Text = "Threshold";
+            this.thresholdToolStripMenuItem.Click += new System.EventHandler(this.thresholdToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(162, 7);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 323);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(272, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(297, 323);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(575, 27);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(301, 323);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 357);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(876, 20);
+            this.textBox1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 261);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(876, 398);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.trackBar1);
             this.MainMenuStrip = this.menuStrip1;
@@ -184,10 +200,10 @@
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,14 +217,17 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem imageProcessingToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.ToolStripMenuItem greyScaleToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem extractRedChannelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thresholdToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- imageprocessing/Form1.cs
+++ imageprocessing/Form1.cs
@@ -18,6 +18,9 @@ namespace imageprocessing
 {
     public partial class Form1 : Form
     {
+        Bitmap sampleImage;
+        Bitmap pimage;
+        Bitmap resizedImage;
         public Form1()
         {
             InitializeComponent();
@@ -27,8 +30,8 @@ namespace imageprocessing
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                pictureBox1.Image = System.Drawing.Image.FromFile(openFileDialog1.FileName);
-
+                sampleImage = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = sampleImage;
             }
         }
 
@@ -41,25 +44,137 @@ namespace imageprocessing
         {
 
             Sepia g = new Sepia();
-            pictureBox2.Image = g.Apply((Bitmap)pictureBox1.Image);
+            pictureBox2.Image = g.Apply((Bitmap)sampleImage);
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            pictureBox2.Image.Save(@"E:\Pranavpic.bmp", ImageFormat.Bmp);
+            pictureBox3.Image.Save(@"E:\Pranavpic.bmp", ImageFormat.Bmp);
         }
 
         private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
         {
+
             Grayscale s = new Grayscale(0.2126, 0.7152, 0.0722);
-            pictureBox2.Image = s.Apply((Bitmap)pictureBox1.Image);
+            pictureBox2.Image = s.Apply((Bitmap)sampleImage);
         }
 
         private void extractRedChannelToolStripMenuItem_Click(object sender, EventArgs e)
         {
             ExtractNormalizedRGBChannel n = new ExtractNormalizedRGBChannel();
-            pictureBox2.Image = n.Apply((Bitmap)pictureBox1.Image);
+            pictureBox2.Image = n.Apply((Bitmap)sampleImage);
+        }
+
+        private void thresholdToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int[] a = new int[pictureBox2.Height];
+            String sum = "";
+            Array.Clear(a, 0, pictureBox2.Height);
+            Grayscale s = new Grayscale(0.2126, 0.7152, 0.0722);
+            pictureBox2.Image = s.Apply((Bitmap)sampleImage);
+            Threshold t = new Threshold(128);
+            pictureBox2.Image = t.Apply((Bitmap)pictureBox2.Image);
+            pimage = new Bitmap(pictureBox2.Image);
+            ResizeBilinear b = new ResizeBilinear(200, 200);
+            resizedImage = b.Apply((Bitmap)pimage);
+
+
+            for (int y = 0; y < resizedImage.Height; y++)
+            {
+                for (int x = 0; x < resizedImage.Width; x++)
+                {
+
+                    Color pixel = resizedImage.GetPixel(x, y);
+                    String hexc = System.Drawing.ColorTranslator.ToHtml(pixel);
+                    if (hexc.Equals("#000000"))
+                    {
+                        a[y]++;
+                    }
+
+                }
+            }
+
+            for (int l = 0; l < a.Length; l++)
+            {
+
+                sum += Convert.ToString(a[l]);
+                sum += " ";
+            }
+
+            textBox1.Text = sum;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 0)
+                {
+
+                    for (int d = 0; d < resizedImage.Width; d++)
+                    {
+                        if (i < resizedImage.Width)
+                        {
+                            resizedImage.SetPixel(d, i, Color.Red);
+                        }
+
+                    }
+                }
+
+                pictureBox3.Image = resizedImage;
+            }
+
+
+          
+            
+
+
+
+            int[] c = new int[resizedImage.Width];
+            for (int y = 0; y < resizedImage.Width; y++)
+            {
+                for (int x = 0; x < resizedImage.Height; x++)
+                {
+
+                    Color pixel = resizedImage.GetPixel(x, y);
+                    String hexc = System.Drawing.ColorTranslator.ToHtml(pixel);
+                    if (hexc.Equals("#000000"))
+                    {
+                        c[y]++;
+                    }
+
+                }
+            }
+
+            for (int l = 0; l < a.Length; l++)
+            {
+
+                sum += Convert.ToString(a[l]);
+                sum += " ";
+            }
+
+            textBox1.Text = sum;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 0)
+                {
+
+                    for (int d = 0; d < resizedImage.Height; d++)
+                    {
+                        if (i < resizedImage.Width)
+                        {
+                            resizedImage.SetPixel(i, d, Color.Red);
+                        }
+
+
+                    }
+                }
+            }
+            pictureBox3.Image = resizedImage;
+        }
+       
+
         }
     }
-}
-       
+
+
+    
